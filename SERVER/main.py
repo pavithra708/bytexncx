2bb7068810d7cd87f62896593b0947bf615a0b89
pavithra708 <pavithrarevankar70@gmail.com>
2025-03-10 18:32:52 +0530

    Update main.py

--- SERVER/main.py
+++ SERVER/main.py
@@ -3,4 +3,5 @@ import os
 
 load_dotenv()  # Load environment variables
 
-print("DATABASE_URL:", os.getenv("postgresql://db_owner:npg_OaNd3pkehs0l@ep-lucky-waterfall-a81kw2u8-pooler.eastus2.azure.neon.tech/db?sslmode=require"))  # Debugging: Check if it loads
+print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+  # Debugging: Check if it loads
